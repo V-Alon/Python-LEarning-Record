40fc7025ead607f593d85b7fd8fad88e38baeffd
V-Alon <15664753579@163.com>
2025-02-27 17:39:33 +0800

    day03-learning
    第三天学习记录

---
+++ day02/实例3-9转义字符的使用.py
@@ -0,0 +1,16 @@
+print('西安')#print自带一个换行    ---   \n
+print('欢迎你！！！')
+print('------------')
+print('西安\n欢迎你！！！')#\n的作用，遇到换行，可以换多行
+print('西\n安\n欢\n迎\n你')
+print('------------')
+print('西安\t欢迎你')#\t制表符
+print('西安西安\t欢迎你')
+print('hello\toooo')#hello是五个字符，一个制表位是八个字符剩下三个空着
+print('hellooooo')
+print('老师说:\'好好学习天天向上\'')
+print('老师说:\"好好学习天天向上\"')
+
+#原子符，使转义字符失效的符号r或者R
+print(r'西\n安\n欢\n迎\n你')
+print(R'西\n安\n欢\n迎\n你')#\n相当于普通字符
