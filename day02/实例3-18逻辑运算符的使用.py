2725b9a456303d416f05de29b14702ccfd33a3db
V-Alon <15664753579@163.com>
2025-02-27 20:55:31 +0800

    day03-learning
    第三天学习记录

---
+++ day02/实例3-18逻辑运算符的使用.py
@@ -0,0 +1,15 @@
+#and，or，not    与或非
+print(True and True)#1
+print(True and False)#0
+print(False and False)#0
+print('-'*40)
+print(9<2 and 9>1)#0
+print(8>7 and 6<5)#0
+
+
+
+print(True or False)#1
+print(True or True)#1
+print(False or False)#0
+print(not False)#1
+print(not True)#0
