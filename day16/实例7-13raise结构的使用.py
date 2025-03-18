76aaccb0977f6e6930596c8da552a0129aef0290
V-Alon <15664753579@163.com>
2025-03-18 18:07:42 +0800

    day16-learning

--- day16/实例7-13raise结构的使用.py
+++ day16/实例7-13raise结构的使用.py
@@ -1,2 +1,10 @@
 #raise的关键字语法的结构为
 #    raise [Exception类型(异常描述信息)]
+try:
+    gender=input('请输入你的性别')
+    if gender !='man' and gender !='woman':
+        raise Exception('性别只能是男或者女')
+except Exception as e:
+    print(e)
+else:
+    pass
---
+++ day16/实例7-14常见异常的产生.py
@@ -0,0 +1,29 @@
+# ZeroDivisionError: division by zero
+# print(10/0)
+
+#IndexError: list index out of range
+# lst=[10,20,30,40]
+# print(lst[4])
+
+#KeyError: 'gender'
+# d={'name':'mkk','age':20}
+# print(d['gender'])
+
+#NameError
+# print(hello)
+
+#SyntaxError: unterminated string literal (detected at line 16)
+# print('hello world)
+
+#ValueError: invalid literal for int() with base 10: 'a'
+# print(int('a'))
+
+#AttributeError: 'int' object has no attribute 'name'
+# i=10
+# print(i.name)
+
+#TypeError: can only concatenate str (not "int") to str
+# print('hello world'+123)
+
+#IndentationError: unexpected indent
+#    print('hello')
