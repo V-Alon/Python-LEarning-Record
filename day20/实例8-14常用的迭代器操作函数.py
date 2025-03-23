2f128fe4f1b2a167e9a58856f9ae5fe35bc4dcca
V-Alon <15664753579@163.com>
2025-03-23 17:31:54 +0800

    day20-learning

--- day20/实例8-14常用的迭代器操作函数.py
+++ day20/实例8-14常用的迭代器操作函数.py
@@ -9,3 +9,59 @@ next(iter)获取迭代器的下一个对象
 filter(function,iter)通过指定条件过滤序列并且返回一个迭代器对象
 map(function,iter)通过函数function对可迭代对象iter的操作返回一个迭代器对象
 '''
+lst = [1,2,3,4,5,6,7,8,9]
+#排序
+asc_lst=sorted(lst)
+desc_lst=sorted(lst,reverse=True)
+print(asc_lst)
+print(desc_lst)
+#反向
+new_lst=reversed(lst)#<class 'list_reverseiterator'>
+print(type(new_lst))
+print(list(new_lst))
+
+
+#zip
+x=['a','b','c','d']
+y=[10,20,30,40,50]
+zipobj=zip(x,y)
+print(type(zipobj))
+# print(list(zipobj))
+
+
+#enumerate
+enum=enumerate(y,start=1)
+print(type(enum))
+
+print(tuple(enum))
+
+
+
+#all
+lst2=[10,20,30,'']
+print(all(lst2))#空字符串为false
+print(any(lst))
+
+
+#any
+print(any(lst2))
+
+#next
+print(next(zipobj))
+print(next(zipobj))
+print(next(zipobj))
+print(next(zipobj))
+
+
+def fun(num):
+    return num%2==1
+obj=filter(fun,range(10))#将range（0-9）整数，都执行一次fun操作
+print(list(obj))
+
+
+def upper(x):
+    return x.upper()
+
+new_lst2=['hello','world','python']
+obj2=map(upper,new_lst2)
+print(list(obj2))
---
+++ day20/实例8-15其他常用内置函数.py
@@ -0,0 +1,7 @@
+'''
+format(value,format_spec)将value以format_spec的格式进行显示
+len(s)获取s的长度或者s元素的个数
+id(obj)获取对象的内存地址
+type(x)获取x的数据类型
+eval(s)执s的字符串所表示的代码
+'''
