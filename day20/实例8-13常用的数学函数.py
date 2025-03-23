436a1406f676724fbe3d7cbab4ce60a01b990cdb
V-Alon <15664753579@163.com>
2025-03-23 17:07:19 +0800

    day20-learning

---
+++ day20/实例8-13常用的数学函数.py
@@ -0,0 +1,21 @@
+'''
+abs(x)获取x的绝对值
+divmod(x, y)获取x和y的商和余数
+max(sequence)获取sequence的最大值
+min(sequence)获取sequence的最小值
+sum(iter)对可迭代对象进行求和
+pow(x, y)获取x的y次幂
+round(x,d)对x进行保留d位小数，结果四舍五入
+'''
+print(abs(-2))
+print(abs(2))
+print(divmod(2, 3))
+print(max('hello'))
+print(min('hello'))
+print(max([1,2,3,4]))
+print(sum([1,2,3,4]))
+print(pow(2, 3))
+print(round(3.14))#round函数只有一个参数与，保留整数
+print(round(3.1415926,3))
+print(round(314.15926,-1))#负数对个位进行舍去
+print(round(314.15926,-2))#负数对十位个位进行舍去
---
+++ day20/实例8-14常用的迭代器操作函数.py
@@ -0,0 +1,11 @@
+'''
+sorted(iter)对可迭代对象进行排序
+reversed(sequence)反转序列生成新的迭代器对象
+zip(iter1, iter2)将iter1和iter2打包成元组并且返回一个可迭代的zip对象
+enumeraate(iter)根据iter对象创建一个enumerate对象
+all(iter)判断可迭代对象iter中所有元素的布尔值是否都为Ture
+any(iter)判断可迭代对象iter中所有元素的布尔值是否都为Ture
+next(iter)获取迭代器的下一个对象
+filter(function,iter)通过指定条件过滤序列并且返回一个迭代器对象
+map(function,iter)通过函数function对可迭代对象iter的操作返回一个迭代器对象
+'''
