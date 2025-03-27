7e081f2452cd5657ff7a0ca445992a4fa969f0d2
V-Alon <15664753579@163.com>
2025-03-27 20:50:35 +0800

    day24-learning

---
+++ day24/实例9-19类的深拷贝和浅拷贝.py
@@ -0,0 +1,20 @@
+class CPU():
+    pass
+class Disk():
+    pass
+class Computer():
+    def __init__(self, disk,cpu):
+        self.disk = disk
+        self.cpu = cpu
+
+
+cpu = CPU()
+disk = Disk()
+
+#创建一台计算计对象
+com = Computer(disk,cpu)
+
+com1=com
+
+print(com,'子对象的内存地址',com.cpu,com.disk)
+print(com1,'子对象的内存地址',com1.cpu,com1.disk)
