409fc72f4d53bc7b493db61b19cf2eb8b08417d4
V-Alon <15664753579@163.com>
2025-03-28 17:31:33 +0800

    day25-learning

--- day25/实战-定义学生类录入5个学生信息存储在列表中.py
+++ day25/实战-定义学生类录入5个学生信息存储在列表中.py
@@ -4,3 +4,26 @@ class Student(object):
         self.age = age
         self.gender = gender
         self.score = score
+
+    def info(self):
+        print(self.name,self.age,self.gender,self.score)
+print("请输入五位学生的信息:(姓名#年龄#性别#成绩)")
+lst=[]
+for i in range(5):
+    s=input(f'请输入第{i}位学生信息及成绩:')
+    s_lst=s.split('#')#索引为0的是姓名，为1是年龄，为2是性别，为3是成绩
+    #创建学生对象
+    stu=Student(s_lst[0],s_lst[1],s_lst[2],s_lst[3])
+    #将学生对象添加到列表中
+    lst.append(stu)
+#遍历列表，调用info方法
+for item in lst:
+    item.info()
+'''
+lhb#24#女#99
+pzh#23#男#90
+mkk#23#男#88
+劳大#24#女#24
+牢底#28#男#69
+
+'''
