971e7546abc2a7fe30414e906563f50aebbf39a8
V-Alon <15664753579@163.com>
2025-03-28 17:03:10 +0800

    day25-learning

--- day24/实例9-19类的深拷贝和浅拷贝.py
+++ day24/实例9-19类的深拷贝和浅拷贝.py
@@ -20,14 +20,16 @@ print(com,'子对象的内存地址',com.cpu,com.disk)
 print(com1,'子对象的内存地址',com1.cpu,com1.disk)
 
 
-
-
-
-
-
 #类对象的浅拷贝
 print('-'*50)
 import copy
-com2=copy.copy(com)
+com2=copy.copy(com)#com2是新产生的对象，com2的子对象，cpu，disk不变
+print(com,'子对象的内存地址是：',com.cpu,com.disk)
+print(com2,'子对象的内存地址是：',com2.cpu,com.disk)
+
+#类对象的深拷贝
+print('-'*50)
+import copy
+com3=copy.deepcopy(com)#com3是新产生的对象，com3的子对象，cpu，disk也会重新产生
 print(com,'子对象的内存地址是：',com.cpu,com.disk)
 print(com2,'子对象的内存地址是：',com2.cpu,com.disk)
