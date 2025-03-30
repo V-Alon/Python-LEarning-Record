c0b7f63242a965ef45b02a65011d8cdd6997abe8
V-Alon <15664753579@163.com>
2025-03-30 14:23:49 +0800

    day27-learning

---
+++ day27/module_a.py
@@ -0,0 +1,7 @@
+# print('welcome to xi`an')
+# name='mkk'
+# print(name)
+if __name__ == '__main__':
+    print('welcome to xi`an')
+    name = 'mkk'
+    print(name)
---
+++ day27/module_b.py
@@ -0,0 +1,4 @@
+#导入的代码
+import module_a
+#在module_b中导入module_a会直接执行a中的内容
+#写上主程序，main，在b中导入a时就不会直接执行a的内容
