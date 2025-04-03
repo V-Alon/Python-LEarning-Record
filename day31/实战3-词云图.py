17391b0ce05386b7deee587a90620fbabf9cd288
V-Alon <15664753579@163.com>
2025-04-03 20:47:50 +0800

    day31-learning

--- day31/实战3-词云图.py
+++ day31/实战3-词云图.py
@@ -4,13 +4,13 @@ from wordcloud import WordCloud
 with open('华为笔记本.txt','r',encoding='utf-8') as file:
     s=file.read()
 #分词
-lst=jieba.lcut
+lst=jieba.lcut(s)
 #排除词
 stopword=['散热性能','外形外观','轻薄程度','其他特色','屏幕效果']
 
 txt=''.join(lst)
 #绘制词云图
-wordcloud = WordCloud(background_color='white',font_path='mysyh.ttc',stopwords=stopword,width=800,height=600)
+wordcloud = WordCloud(background_color='white',font_path='msyh.ttc',stopwords=stopword,width=800,height=600)
 
 wordcloud.generate(txt)
 
