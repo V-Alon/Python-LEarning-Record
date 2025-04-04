f3adfaf2d3ebdd81551c55f033d60657041483c7
V-Alon <15664753579@163.com>
2025-04-04 21:24:03 +0800

    day32-learning

--- day32/实例11-1文件的读写操作.py
+++ day32/实例11-1文件的读写操作.py
@@ -10,3 +10,19 @@
 关闭文件
 关闭文件.close()
 '''
+def my_write():
+    file=open('a.txt','w',encoding='utf-8')
+    file.write('伟大的中国梦')
+    file.close()
+
+def my_read():
+    file=open('a.txt','r',encoding='utf-8')
+    s=file.read()
+    print(s)
+    print(type(s))
+    file.close()
+# if __name__ == '__main__':
+#     my_write()
+
+if __name__ == '__main__':
+    my_read()
