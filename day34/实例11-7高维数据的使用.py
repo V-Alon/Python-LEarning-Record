5f16852484d2ba313296cb583992902030cf5f8d
V-Alon <15664753579@163.com>
2025-04-06 21:53:52 +0800

    day34-learning

--- day34/实例11-7高维数据的使用.py
+++ day34/实例11-7高维数据的使用.py
@@ -1,1 +1,10 @@
 #JSON模块
+import json
+lst=[
+    {'name':'mkk','age':19,'score':100},
+    {'name':'xxx','age':20,'score':99},
+    {'name':'vvv','age':22,'score':98},
+]
+#使用dumps
+s=json.dumps(lst,ensure_ascii=False,indent=4)
+
