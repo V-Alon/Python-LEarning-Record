4d429d4b4d903b241bc8e975832170dac089986a
V-Alon <15664753579@163.com>
2025-04-06 21:38:55 +0800

    Update 实例11-6一维数据和二维数据的存储.py

--- day34/实例11-6一维数据和二维数据的存储.py
+++ day34/实例11-6一维数据和二维数据的存储.py
@@ -4,5 +4,14 @@ def my_write():
     with open('studeen.csv','w',encoding='utf-8') as file:
         file.write(','.join(lst))#转成字符串
 
+def my_read():
+    with open('studeen.csv','r',encoding='utf-8') as file:
+        s=file.read()
+        lst=s.split(',')
+        print(lst)
+
+
+
 if __name__ == '__main__':
-    my_write()
+    # my_write()
+    my_read()
