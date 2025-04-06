18bd47c74625ea234fe92ec3cb8bcd60a0afee2b
V-Alon <15664753579@163.com>
2025-04-06 21:50:10 +0800

    day34-learning

--- day34/实例11-6一维数据和二维数据的存储.py
+++ day34/实例11-6一维数据和二维数据的存储.py
@@ -11,7 +11,30 @@ def my_read():
         print(lst)
 
 
+def my_write_table():
+    lst=[
+        ['商品名称','单价','采购数量'],
+        ['水杯','98.5','20'],
+        ['鼠标','89','100']
+    ]
+    with open('table.csv','w',encoding='utf-8') as file:
+        for item in lst:
+            line=','.join(item)
+            file.write(line)
+            file.write('\n')
 
+
+def my_read_table():
+    data=[]
+    with open('table.csv','r',encoding='utf-8') as file:
+        lst=file.readlines()
+        # print(s)
+        for item in lst:
+            new_lst=item[:len(item)-1].split(',')
+            data.append(new_lst)
+    print(data)
 if __name__ == '__main__':
     # my_write()
-    my_read()
+    # my_read()
+    # my_write_table()
+    my_read_table()
