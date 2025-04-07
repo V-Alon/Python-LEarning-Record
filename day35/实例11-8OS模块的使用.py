3afe0c23df13c502f2dbc68b96dcb2c5517c44c5
V-Alon <15664753579@163.com>
2025-04-07 21:30:46 +0800

    day35-learning

---
+++ day35/实例11-8OS模块的使用.py
@@ -0,0 +1,11 @@
+import os
+print(os.getcwd())
+lst=os.listdir()
+print('当前路径下所有文件目录',lst)
+print('当前路径下所有文件目录',os.listdir('D:\GitHub-works\Python-LEarning-Record'))
+#创建目录
+# os.mkdir('hello')#已经存在会报错[WinError 183] 当文件已存在时，无法创建该文件。: 'hello'
+# os.makedirs('./aa/bb/cc')
+#删除（空）目录
+# os.rmdir('hello')#要删除的目录不存在也报错
+# os.removedirs('./aa/bb/cc')
