8038997328e9d63bfc22ec5c2ad3004b974baac4
V-Alon <15664753579@163.com>
2025-04-08 19:22:36 +0800

    day36-learning

--- day36/实战3记录用户日志并且查看.py
+++ day36/实战3记录用户日志并且查看.py
@@ -11,7 +11,36 @@ def raed_loginfo():
     with open('logs.txt', 'r', encoding='utf-8') as file:
         while True:
             line = ''
-            
+            line = file.readline()
+            if line=='':
+                break
+            else:
+                print(line)
+
 
 if __name__ == '__main__':
-    write_loginfo('admin')
+    # write_loginfo('admin')
+    username=input('请输入用户名:')
+    pwd=input('请输入密码:')
+    if username=='admin' and pwd=='admin':
+        print('success')
+
+        #
+        write_loginfo(username)
+        show_info()
+        num=eval(input('请输入要操作的数字:'))
+        while True:
+            if num==0:
+                print('退出成功')
+                break
+            elif num==1:
+                print('查看登录日志')
+                raed_loginfo()
+                show_info()
+            else:
+                print('error')
+                show_info()
+            num=eval(input('请输入要操作的数字:'))
+
+    else:
+        print('用户名或者密码不正确')
