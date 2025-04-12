118a34a4bdc9dcdf6f3cf77e2f1c292b2adf9b95
V-Alon <15664753579@163.com>
2025-04-12 20:22:04 +0800

    day40-learning

---
+++ day40/实例13-6使用Pool进程池-阻塞方式.py
@@ -0,0 +1,19 @@
+from multiprocessing import Pool
+import time,os
+def task(name):
+    print(f'子进程的PID:{os.getpid()}，执行的任务:{name}')
+    time.sleep(1)
+
+if __name__ == '__main__':
+    #主进程
+    start = time.time()
+    print('父进程开始执行')
+    p=Pool(3)
+    #创建任务
+    for i in range(10):
+        p.apply(func=task,args=(i,))
+
+    p.close()
+    p.join()#阻塞
+    print('所有子进程执行完毕，父进程执行完毕')
+    print(time.time()-start)
