fdaf98b9c3d61e8783c868fa75c6bfa1a7d81649
V-Alon <15664753579@163.com>
2025-04-14 17:32:58 +0800

    day42-learning

--- day42_多人聊天/server.py
+++ day42_多人聊天/server.py
@@ -1,4 +1,6 @@
 #coding:utf-8
+from socket import socket,AF_INET,SOCK_STREAM
+
 import wx
 class KkServer(wx.Frame):
     def __init__(self):
@@ -22,21 +24,38 @@ class KkServer(wx.Frame):
         #
         box.Add(fgz1,1,wx.ALIGN_CENTER)
 
-
         # 只读文本框
         self.show_text = wx.TextCtrl(pl, size=wx.Size(400, 410), style=wx.TE_MULTILINE | wx.TE_READONLY)
         box.Add(self.show_text, 1, wx.ALIGN_CENTER)
-
         #
         pl.SetSizer(box)
+        '''------------------------以上的代码都是界面绘制的代码----------------------'''
+        '''服务器功能实现必要属性'''
+        self.isOn=False#服务器启动状态，默认为0，没有启动
+        # 服务器绑定的ip和端口号
+        self.host_port=('',8888)#空字符串代表本机所以的IP
+        #创建socket对象
+        self.server_socket=socket(AF_INET,SOCK_STREAM)
+        #绑定IP和端口号
+        self.server_socket.bind(self.host_port)
+        #监听
+        self.server_socket.listen(5)
+        #创建字典存储客户端对话线程
+        self.session_thread_dict={}#key-value{客户端的名称key:会话线程:value}
+
+        #当鼠标点击按钮执行的操作
+        self.Bind(wx.EVT_BUTTON,self.start_server,start_server_btn)
+
+
+    def start_server(self,event):
+        print('正在启动服务........')
+        print('启动完毕')
 
 if __name__ == '__main__':
-    if __name__ == '__main__':
-        # 初始化app
-        app = wx.App()
-        # 创建自己的服务器端界面对象
-        client = KkServer()
-        client.Show()
+    # 初始化app
+    app = wx.App()
+    # 创建自己的服务器端界面对象
+    client = KkServer()
+    client.Show()
 
-        app.MainLoop()
-        
+    app.MainLoop()
