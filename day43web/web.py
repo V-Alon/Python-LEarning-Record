97e5b0e207c3f03c0f3f90d34f2b9b551368a941
V-Alon <15664753579@163.com>
2025-04-15 21:09:03 +0800

    day43-learning

--- day43web/web.py
+++ day43web/web.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 #创建的网址/show/info和函数index的对应关系
 #在浏览器上访问/show/info，网站自动执行index
 @app.route('/show/info')
 def index():
-    return "Hello World!"
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
