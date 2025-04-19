d555142d7cfbc356cec96e3283014b5c6f00735c
V-Alon <15664753579@163.com>
2025-04-19 20:47:13 +0800

    day47-learning

--- day47/bs4的基本使用01.py
+++ day47/bs4的基本使用01.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 url='http://price.cnveg.com/'
 resp=requests.get(url)
-print(resp.text)
+# print(resp.text)
 
 #解析数据
 #把页面源代码
@@ -15,3 +15,14 @@ page=BeautifulSoup(resp.text,'html.parser')#指定html信息
 # page.find("table",class_="border2 m_t_8")#class是python的关键字
 table = page.find("table",attrs={"class":"border2 m_t_8"})#和上面一个意思，但是可以避免class关键字
 print(table)
+#拿到所有数据
+trs=table.find_all('tr')[1:]
+for tr in trs:#每一行
+    tds=tr.find_all('td')
+    name=tds[0].text
+    low=tds[1].text
+    avg=tds[2].text
+    high=tds[3].text
+    kind=tds[4].text
+    date=tds[5].text
+    print(name,low,avg,high,kind,date)
