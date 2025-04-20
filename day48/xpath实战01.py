d4620b29b63b3a757a628b1a3da2f58f2ff1239a
V-Alon <15664753579@163.com>
2025-04-20 21:59:41 +0800

    day48-learning

--- day48/xpath实战01.py
+++ day48/xpath实战01.py
@@ -1,5 +1,7 @@
 #拿到页面源代码
 #提取和解析数据
+from http.cookiejar import request_path
+
 import requests
 from lxml import etree
 
@@ -8,3 +10,11 @@ resp=requests.get(url)
 print(resp.text)
 
 html=etree.HTML(resp.text)
+divs=html.xpath("https://www.zbj.com/?fr=frzpheader")
+for div in divs:
+    div.xpath(".//*[@id='content']/ul/li")
+    price=div.xpath(".//*[@id='content']/ul/li/span/text()")[0].strip()#去掉列表的方框和引号，只要价格的数字
+    title='saas'.join(div.xpath(".//*[@id='content']/ul/li/span/text()"))
+    com_name=div.xpath(".//*[@id='content']/ul/li/span/text()")
+    location=div.xpath(".//*[@id='content']/ul/li/span/text()")
+    print(price,title,com_name,location)
