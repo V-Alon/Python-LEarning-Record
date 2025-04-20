c35ab3c4271c6eaa44e3b99d65a3620572bed4d1
V-Alon <15664753579@163.com>
2025-04-20 21:17:53 +0800

    day48-learning

--- day48/Xpath解析入门01.py
+++ day48/Xpath解析入门01.py
@@ -11,14 +11,22 @@ xml='''
     <price>1.23</price>
     <nick>臭豆腐</nick>
     <author>
-        <nick id="10086">周杰伦</nick>
+        <nick id="10086">周大强</nick>
         <nick id="10010">周芷若</nick>
         <nick class="joy">周杰伦</nick>
         <nick class="jolin">蔡依林</nick>
         <div>
-            <nick>惹了</nick>
+            <nick>惹了1</nick>
         </div>
+        <span>
+            <nick>惹了2</nick>
+        </span>
     </author>
+    author/div/nick
+    author/span/nick
+    
+    
+    
     <partner>
         <nick id="pcc">ppc</nick>
         <nick id="ppbc">ppbc</nick>
@@ -28,5 +36,8 @@ xml='''
 tree=etree.XML(xml)
 #result=tree.xpath('/book')#/代表层级关系。第一个/ 是根节点
 # result =tree.xpath('/book/name')
-result =tree.xpath('/book/name/text()')
+# result =tree.xpath('/book/name/text()')#text()拿文本
+# result =tree.xpath('/book/author//nick/text()')#//两个“/”后代
+# result =tree.xpath('/book/author/*/nick/text()')#   * 任意节点.通配符
+result=tree.xpath('/book//nick/text()')
 print(result)
---
+++ day48/xpath解析入门02.py
@@ -0,0 +1,16 @@
+from lxml import etree
+tree = etree.parse('b.html')
+# result = tree.xpath('/html')
+# result = tree.xpath('/html/body/ul/li/a/text()')
+# result = tree.xpath('/html/body/ul/li[1]/a/text()')#[数字]选择性的提取哪几个               xpath的顺序是从1开始的
+# result = tree.xpath("/html/body/ol/li/a[@href='dapao']/text()")#[@xxx=xxx]属性的筛选
+# print(result)
+ol_li_list=tree.xpath('/html/body/ol/li')
+for li in ol_li_list:
+    #从每一个li中提取文字信息
+    t=li.xpath('./a/text()')#继续去寻找，相对查找
+    print(t)
+    result2=li.xpath('./a/@href')#@拿到属性值，@属性
+    print(result2)
+
+
