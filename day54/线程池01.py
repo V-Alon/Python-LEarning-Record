6fc49df0c0f1a84c5e3871630632d20ab888f99b
V-Alon <15664753579@163.com>
2025-04-27 20:42:34 +0800

    day54-learning

--- day54/线程池01.py
+++ day54/线程池01.py
@@ -2,6 +2,7 @@
 #上线程池，多个页面同时抓取
 import requests,csv
 from lxml import etree
+from concurrent.futures import ProcessPoolExecutor,ThreadPoolExecutor
 
 from day46.豆瓣top250 import csv_writer
 f=open('datacsv.csv',mode='w',encoding='utf-8')
@@ -21,4 +22,9 @@ def download_one_page(url):
 
 
 if __name__ == '__main__':
-    download_one_page('http://www.shucai123.com/price/')
+
+    # for i in range(1,14870):
+    #     download_one_page(f'http://www.shucai123.com/price/{i}.html')
+    with ThreadPoolExecutor(max_workers=50) as t:
+        for i in range(1, 200):
+            t.submit(download_one_page,f'http://news.163.com/news?page={i}')
