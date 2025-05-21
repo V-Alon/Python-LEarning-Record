4959b3e6e27f49b6ca83c72f98232bd1433a00e0
V-Alon <15664753579@163.com>
2025-05-21 21:26:54 +0800

    day75-learning

--- day74/numpy.argmax和min的使用方法.py
+++ day74/numpy.argmax和min的使用方法.py
@@ -37,23 +37,35 @@ min
 '''
 import numpy as np
 
+
+
+
 # 创建一个一维数组
 arr1d = np.array([3, 1, 4, 1, 5, 9, 2, 6, 5])
 min_index_1d = np.argmin(arr1d)
 print("一维数组最小值的索引：", min_index_1d)  # 输出：1
+
+
+
 
 # 创建一个二维数组
 arr2d = np.array([[7, 2, 9],
                   [4, 6, 3],
                   [8, 1, 5]])
 
+
+
 # 不指定axis时，将数组展平为一维数组后查找索引
 min_index_flat = np.argmin(arr2d)
 print("二维数组展平后的最小值索引：", min_index_flat)  # 输出：7
 
+
+
 # 沿着行的方向（axis=0）查找每列的最小值索引
 min_indices_axis0 = np.argmin(arr2d, axis=0)
 print("每列最小值的索引：", min_indices_axis0)  # 输出：[1 2 1]
+
+
 
 # 沿着列的方向（axis=1）查找每行的最小值索引
 min_indices_axis1 = np.argmin(arr2d, axis=1)
