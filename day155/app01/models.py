1b516b99ad4e1ee39a8405ece7d91ac65e379aa4
V-Alon <15664753579@163.com>
2025-08-20 12:29:49 +0800

    day155

---
+++ day155/app01/models.py
@@ -0,0 +1,34 @@
+
+
+from django.db import models
+
+# Create your models here.
+class Department(models.Model):
+    """部门表"""
+    # id = models.BigAutoField(primary_key=True,verbose_name="ID")
+    title = models.CharField(max_length=32,verbose_name="标题")
+
+class UserInfo(models.Model):
+    """员工表"""
+    name = models.CharField(verbose_name="姓名",max_length=16)
+    password = models.CharField(verbose_name="密码",max_length=64)
+    age = models.IntegerField(verbose_name="年龄")
+    account = models.DecimalField(verbose_name="账户余额",max_digits=10,decimal_places=2,default=0)
+    create_time = models.DateTimeField(verbose_name="入职时间")
+
+
+    #无约束
+    # depart_id = models.BigAutoField(verbose="部门ID")
+
+    #1.有约束
+    # --to 与那个表进行关联
+    # --to_field 与哪一列做关联
+    #2.django自动
+    # -- 写的depart
+    # --生成数据列 depart_id
+    # 3. 部门被删除
+    # --1.级联删除
+    # depart = models.ForeignKey(to=Department, to_field="id", on_delete=models.CASCADE)
+    # --2.设置成空的
+    #    depart = models.ForeignKey(to=Department,to_field="id",null=True,blank=True,on_delete=models.SET_NULL)
+    depart = models.ForeignKey(to=Department,to_field="id",null=True,blank=True,on_delete=models.SET_NULL)
