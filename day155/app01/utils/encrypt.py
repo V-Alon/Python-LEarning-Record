6595dd99aca2f4c301677de91fa59cf796105b0c
V-Alon <15664753579@163.com>
2025-08-27 17:37:07 +0800

    day162

--- day155/app01/utils/encrypt.py
+++ day155/app01/utils/encrypt.py
@@ -1,7 +1,8 @@
+from django.conf import settings
 import hashlib
 
 def md5(data_string):
     salt = "XXXXXX"
-    obj = hashlib.md5(salt.encode("utf-8"))
+    obj = hashlib.md5(settings.SECRET_KEY.encode('utf-8'))
     obj.update(data_string.encode('utf-8'))
     return obj.hexdigest()
--- day155/app01/views/admin.py
+++ day155/app01/views/admin.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render,redirect
-
 from app01 import models
 from app01.utils.pagination import Pagination
 
@@ -21,6 +20,8 @@ def admin_list(request):
 from app01.utils.bootstrap import BootStrapModelForm
 from django import forms
 from django.core.exceptions import ValidationError
+from app01.utils.encrypt import md5
+
 class AdminModeForm(BootStrapModelForm):
 
     confirm_password = forms.CharField(label="确认密码",widget=forms.PasswordInput)
@@ -31,6 +32,7 @@ class AdminModeForm(BootStrapModelForm):
         widgets = {"password": forms.PasswordInput}
         #可以在PasswordInput后夹砂管加上render_value=True
         #密码校验错误后原来输入的可以保留
+
     def clean_password(self):
         password = self.cleaned_data.get('password')
 
@@ -38,16 +40,43 @@ class AdminModeForm(BootStrapModelForm):
 
 
 
-        return "qqq"
-
-
-
-
-
+        return md5(password)
 
     def clean_confirm_password(self):
         password = self.cleaned_data.get('password')
-        confirm_password = self.cleaned_data.get('confirm_password')
+        confirm_password = md5(self.cleaned_data.get('confirm_password'))
+        if password != confirm_password:
+            raise ValidationError("密码不一致")
+        #返回什么，保存到数据库就是什么
+        return confirm_password
+
+
+class AdminEditModelForm(BootStrapModelForm):
+    class Meta:
+        model = models.Admin
+        fields = ["username"]
+
+class AdminResetModelForm(BootStrapModelForm):
+    confirm_password = forms.CharField(label="确认密码", widget=forms.PasswordInput)
+    class Meta:
+        model = models.Admin
+        fields = ["password", "confirm_password"]
+        widgets = {"password": forms.PasswordInput}
+
+    def clean_password(self):
+        password = self.cleaned_data.get('password')
+        md5_password = md5(password)
+
+        #去数据库校验和新输入的密码是否相同
+        exists = models.Admin.objects.filter(id = self.instance.pk,password=md5_password).exists()
+        if exists:
+            error = "密码不可以和之前一样"
+            raise ValidationError(error)
+        return md5(password)
+
+    def clean_confirm_password(self):
+        password = self.cleaned_data.get('password')
+        confirm_password = md5(self.cleaned_data.get('confirm_password'))
         if password != confirm_password:
             raise ValidationError("密码不一致")
         #返回什么，保存到数据库就是什么
@@ -67,5 +96,46 @@ def admin_add(request):
 
     return render(request, 'change.html', {"title": title, "form": form})
 
+def admin_edit(request,nid):
+    """编辑管理员"""
+    #对象或者None
+    row_obj = models.Admin.objects.filter(id=nid).first()
+    if row_obj is None:
+        error = "!!!ERROR!!!"
+        # return render(request, 'error.html',{"error":error})
+        return redirect('/admin/list/')
 
+    title = "编辑管理员"
 
+    if request.method == "GET":
+        form = AdminEditModelForm(instance=row_obj)
+        return render(request, 'change.html', {"title": title, "form": form})
+
+    form = AdminEditModelForm(data = request.POST, instance=row_obj)
+    if form.is_valid():
+        form.save()
+        return redirect('/admin/list/')
+    return render(request, 'change.html', {"title": title, "form": form})
+
+def admin_delete(request,nid):
+    """删除管理员"""
+    models.Admin.objects.filter(id=nid).delete()
+    return redirect('/admin/list/')
+
+def admin_reset(request,nid):
+    """重置密码"""
+    # 对象或者None
+    row_obj = models.Admin.objects.filter(id=nid).first()
+    if row_obj is None:
+        return redirect('/admin/list/')
+    title = "重置密码 - {}".format(row_obj.username)
+
+    if request.method == "GET":
+        form = AdminResetModelForm()
+        return render(request, 'change.html', {"title": title, "form": form})
+
+    form = AdminResetModelForm(data = request.POST, instance=row_obj)
+    if form.is_valid():
+        form.save()
+        return redirect('/admin/list/')
+    return render(request, 'change.html', {"title": title, "form": form})
