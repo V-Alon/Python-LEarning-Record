999ae03228036391aa66560b48d1dd1bb6cc9e1e
V-Alon <15664753579@163.com>
2025-09-15 17:08:46 +0800

    day180

--- day155/app01/views/order.py
+++ day155/app01/views/order.py
@@ -1,10 +1,14 @@
 import json
+import random
+from datetime import datetime
 from django.shortcuts import render,HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 
 from app01 import models
 from app01.utils.bootstrap import BootStrapModelForm
+
+
 
 class OrderModelForm(BootStrapModelForm):
     class Meta:
@@ -24,10 +28,11 @@ def order_list(request):
 @csrf_exempt
 def order_add(request):
     """创建订单Ajax请求"""
-    form = OrderModelForm(data= request.POST)
+    form = OrderModelForm(data=request.POST)
     if form.is_valid():
+        form.instance.oid = datetime.now().strftime('%Y%m%d%H%M%S') + str(random.randint(1000,9999))
+        #保存到数据库
         form.save()
-        # return HttpResponse(json.dumps({'status': True}))
-        return JsonResponse({'status':True})
+        return JsonResponse({'status': True})
 
-    return JsonResponse({'status': False,"error": form.errors})
+    return JsonResponse({'status': False, 'error': form.errors.get_json_data()})
